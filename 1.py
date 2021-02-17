c3243fa6680cae6addd50029751cf77ad42b4a57
Yangning <792301982@qq.com>
2021-02-17 19:08:03 +0800

    fix:ip获取url

--- 1.py
+++ 1.py
@@ -7,17 +7,18 @@ import hashlib
 import random
 import api
 
-getip_url='http://jsonip.com'
+getip_url='https://ipv4.jsonip.com/'
 
 while(1):
     try:
-        time.sleep(60)
         r=requests.get(getip_url)
         ip=json.loads(r.text)['ip']
         print(ip)
         qcloud = api.QcloudApi()
-        ret = qcloud.get(module='cns', action='RecordModify', domain='yangning.work', recordId=593013342, subDomain='@', value=ip, recordType='A', recordLine='默认')
+        ret = qcloud.get(module='cns', action='RecordModify', domain='yangning.work', recordId=742259884, subDomain='@', value=ip, recordType='A', recordLine='默认')
         print(ret)
+        time.sleep(600)
     except:
         print("失败")
+        time.sleep(600)
         continue
